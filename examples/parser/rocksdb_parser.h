95db3a92af3adefe0bbeaaccc6041d14c1ff4269
Tal Zussman <tz2294@columbia.edu>
2022-10-16 19:29:00 -0400

    Add more contants to rocksdb_parser.h

--- examples/parser/rocksdb_parser.h
+++ examples/parser/rocksdb_parser.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// Footer member sizes
 #define CHECKSUM_LEN 1
 #define MAX_VARINT64_LEN 10
 #define VERSION_LEN 4
@@ -12,8 +13,48 @@
 #define FOOTER_LEN (CHECKSUM_LEN + 2 * MAX_BLOCK_HANDLE_LEN + VERSION_LEN + MAGIC_NUM_LEN)
 #define LEGACY_FOOTER_LEN (2 * MAX_BLOCK_HANDLE_LEN + MAGIC_NUM_LEN)
 
+// Magic numbers
 #define BLOCK_MAGIC_NUMBER 0x88e241b785f4cff7ull // kBlockBasedTableMagicNumber
 #define LEGACY_BLOCK_MAGIC_NUMBER 0xdb4775248b80fb57ull // kLegacyBlockBasedTableMagicNumber
+#define NULL_BLOCK_MAGIC_NUMBER 0
+
+// Checksum parsing
+enum checksum_type {
+    kNoChecksum = 0x0,
+    kCRC32c = 0x1,
+    kxxHash = 0x2,
+    kxxHash64 = 0x3,
+    kXXH3 = 0x4,
+};
+
+#define kInvalidChecksumType ((1 << (8 * sizeof(enum checksum_type)) | kNoChecksum)
+
+static inline int valid_checksum_type(uint8_t checksum_type) {
+    return checksum_type >= kNoChecksum && checksum_type <= kXXH3;
+}
+
+// Format version parsing
+#define kLegacyFormatVersion 0
+#define kLatestFormatVersion 5
+#define kInvalidFormatVersion 0xffffffffU
+
+static inline int valid_format_version(uint32_t version) {
+    return version <= kLatestFormatVersion;
+}
+
+// The index type that will be used for this table
+enum index_type {
+    kBinarySearch = 0x00,
+    kHashSearch = 0x01,
+    kTwoLevelIndexSearch = 0x02,
+    kBinarySearchWithFirstKey = 0x03,
+};
+
+// The index type that will be used for the data block.
+enum datablock_index_type {
+    kDataBlockBinarySearch = 0,   // traditional block type
+    kDataBlockBinaryAndHash = 1,  // additional hash index
+};
 
 struct rocksdb_opts {
     uint32_t magic_num_len; // kMagicNumberLengthByte = 8
