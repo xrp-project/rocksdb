5b4d6cf6961fdb3c64e745c2a8b835a44f8f7d19
Tal Zussman <tz2294@columbia.edu>
2022-10-16 19:28:42 -0400

    Add input checking to varint.c

--- examples/parser/varint.c
+++ examples/parser/varint.c
@@ -17,6 +17,9 @@ int varint64_length(uint64_t n) {
 unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint32_t limit) {
     unsigned char *ptr = dst;
 
+    if (!ptr)
+        return NULL;
+
     while (n >= VARINT64_MSB) {
         *(ptr++) = (n & (VARINT64_MSB - 1)) | VARINT64_MSB;
         n >>= 7;
@@ -32,6 +35,9 @@ unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint32_t limit) {
 const unsigned char *decode_varint64(const unsigned char *src, uint64_t *value, uint32_t limit) {
     uint64_t result = 0;
     const unsigned char *ptr = src;
+
+    if (!ptr || !value)
+        return NULL;
 
     for (uint32_t shift = 0; shift <= 63 && src - ptr < limit; shift += VARINT64_SHIFT) {
         unsigned char byte = *ptr;
