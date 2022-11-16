d087254fbc8b2cee78d76d2f364ca35f59286e84
Tal Zussman <tz2294@columbia.edu>
2022-11-16 06:00:14 -0500

    Add 32-bit variant to varint functions

--- examples/parser/varint.c
+++ examples/parser/varint.c
@@ -2,11 +2,11 @@
 
 #include "varint.h"
 
-int varint64_length(uint64_t n) {
+int varint_length(uint64_t n) {
     int len = 1;
 
-    while (n >= VARINT64_MSB) {
-        n >>= VARINT64_SHIFT;
+    while (n >= VARINT_MSB) {
+        n >>= VARINT_SHIFT;
         len++;
     }
 
@@ -14,14 +14,14 @@ int varint64_length(uint64_t n) {
 }
 
 // Returns pointer to one past end of dst
-unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint32_t limit) {
+unsigned char *encode_varint32(unsigned char *dst, uint32_t n, uint8_t limit) {
     unsigned char *ptr = dst;
 
     if (!ptr)
         return NULL;
 
-    while (n >= VARINT64_MSB) {
-        *(ptr++) = (n & (VARINT64_MSB - 1)) | VARINT64_MSB;
+    while (n >= VARINT_MSB) {
+        *(ptr++) = (n & (VARINT_MSB - 1)) | VARINT_MSB;
         n >>= 7;
         if (ptr - dst >= limit)
             return NULL;
@@ -32,19 +32,62 @@ unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint32_t limit) {
 }
 
 // Returns pointer to one past end of src
-const unsigned char *decode_varint64(const unsigned char *src, uint64_t *value, uint32_t limit) {
+const unsigned char *decode_varint32(const unsigned char *src, uint32_t *value, uint8_t limit) {
+    uint32_t result = 0;
+    const unsigned char *ptr = src;
+
+    if (!ptr || !value)
+        return NULL;
+
+    for (uint8_t shift = 0; shift <= 27 && src - ptr < limit; shift += VARINT_SHIFT) {
+        unsigned char byte = *ptr;
+        ptr++;
+
+        if (byte & VARINT_MSB) {
+            result |= ((byte & (VARINT_MSB - 1)) << shift);
+        }
+        else {
+            result |= (byte << shift);
+            *value = result;
+            return ptr;
+        }
+    }
+
+    return NULL;
+}
+
+// Returns pointer to one past end of dst
+unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint8_t limit) {
+    unsigned char *ptr = dst;
+
+    if (!ptr)
+        return NULL;
+
+    while (n >= VARINT_MSB) {
+        *(ptr++) = (n & (VARINT_MSB - 1)) | VARINT_MSB;
+        n >>= 7;
+        if (ptr - dst >= limit)
+            return NULL;
+    }
+
+    *(ptr++) = n;
+    return ptr;
+}
+
+// Returns pointer to one past end of src
+const unsigned char *decode_varint64(const unsigned char *src, uint64_t *value, uint8_t limit) {
     uint64_t result = 0;
     const unsigned char *ptr = src;
 
     if (!ptr || !value)
         return NULL;
 
-    for (uint32_t shift = 0; shift <= 63 && src - ptr < limit; shift += VARINT64_SHIFT) {
+    for (uint8_t shift = 0; shift <= 63 && src - ptr < limit; shift += VARINT_SHIFT) {
         unsigned char byte = *ptr;
         ptr++;
 
-        if (byte & VARINT64_MSB) {
-            result |= ((byte & (VARINT64_MSB - 1)) << shift);
+        if (byte & VARINT_MSB) {
+            result |= ((byte & (VARINT_MSB - 1)) << shift);
         }
         else {
             result |= (byte << shift);
--- examples/parser/varint.h
+++ examples/parser/varint.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
-#define VARINT64_SHIFT ((unsigned int) 7)
-#define VARINT64_MSB ((unsigned int) (1 << (VARINT64_SHIFT))) // 128 == 0x80
+#define VARINT_SHIFT ((unsigned int) 7)
+#define VARINT_MSB ((unsigned int) (1 << (VARINT_SHIFT))) // 128 == 0x80
 
-int varint64_length(uint64_t n);
-unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint32_t limit);
-const unsigned char *decode_varint64(const unsigned char *src, uint64_t *value, uint32_t limit);
+int varint_length(uint64_t n);
+
+unsigned char *encode_varint32(unsigned char *dst, uint32_t n, uint8_t limit);
+const unsigned char *decode_varint32(const unsigned char *src, uint32_t *value, uint8_t limit);
+
+unsigned char *encode_varint64(unsigned char *dst, uint64_t n, uint8_t limit);
+const unsigned char *decode_varint64(const unsigned char *src, uint64_t *value, uint8_t limit);
 
 #endif // _PARSER_VARINT_H_
