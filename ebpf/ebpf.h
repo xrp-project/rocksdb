95aeaa5b8ab6bb82a9b7dd74a515b84fcab7dfa5
Tal Zussman <tz2294@columbia.edu>
2023-04-04 01:01:06 -0400

    Switch to using full huge page for scratch buffer

--- ebpf/ebpf.h
+++ ebpf/ebpf.h
@@ -2,7 +2,7 @@
 #define _EBPF_EBPF_H_
 
 #define EBPF_DATA_BUFFER_SIZE (4096)
-#define EBPF_SCRATCH_BUFFER_SIZE (4 * 4096)
+#define EBPF_SCRATCH_BUFFER_SIZE (1 << 21) // (4 * 4096)
 #define EBPF_BLOCK_SIZE 512
 
 #define SYS_READ_XRP 445
