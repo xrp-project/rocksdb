2757b9b76ba4c37d266d7b05dd8b96240bc1a6ed
Ioannis Zarkadas <iz2175@columbia.edu>
2023-04-14 00:41:33 +0000

    wip
    Signed-off-by: Ioannis Zarkadas <iz2175@columbia.edu>

--- ebpf/test.c
+++ ebpf/test.c
@@ -20,12 +20,12 @@ const size_t huge_page_size = 1UL << 21UL;
 
 static void die(const char *message) {
     perror(message);
-    exit(1); 
+    exit(1);
 }
 
-static void __attribute__((unused)) print_block_handle(struct block_handle *handle) {
-    if (handle == NULL)
-        return;
+static void __attribute__((unused))
+print_block_handle(struct block_handle *handle) {
+    if (handle == NULL) return;
 
     printf("Offset: %lx\n", handle->offset);
     printf("Size: %lx\n", handle->size);
@@ -70,56 +70,67 @@ int main(int argc, char **argv) {
     if ((sst_fd = open(filename, O_RDONLY | O_DIRECT)) == -1)
         die("open() sst file failed");
 
-    if (fstat(sst_fd, &st) == -1)
-        die("fstat() failed");
+    if (fstat(sst_fd, &st) == -1) die("fstat() failed");
 
-    offset = ((st.st_size - MAX_FOOTER_LEN) / EBPF_BLOCK_SIZE) * EBPF_BLOCK_SIZE;
+    offset = ((st.st_size - MAX_FOOTER_LEN) / EBPF_BLOCK_SIZE) *
+             EBPF_BLOCK_SIZE;
     printf("footer offset (aligned to 512): %ld\n", offset);
 
-    data_buf = mmap(NULL, huge_page_size, PROT_READ | PROT_WRITE, MAP_HUGETLB | MAP_HUGE_2MB | MAP_ANON | MAP_PRIVATE, -1, 0);
-    if (data_buf == MAP_FAILED)
-        die("mmap() failed");
+    // data_buf = mmap(NULL, huge_page_size, PROT_READ | PROT_WRITE, MAP_HUGETLB
+    // | MAP_HUGE_2MB | MAP_ANON | MAP_PRIVATE, -1, 0); if (data_buf ==
+    // MAP_FAILED)
+    //     die("mmap() failed");
 
-    if (posix_memalign((void **) &scratch_buf, EBPF_SCRATCH_BUFFER_SIZE, EBPF_DATA_BUFFER_SIZE) != 0)
-        die("posix_memalign() failed");
+    if (posix_memalign((void **)&data_buf, EBPF_DATA_BUFFER_SIZE,
+                       EBPF_DATA_BUFFER_SIZE) != 0)
+        die("data buf posix_memalign() failed");
 
-    memset(data_buf, 0, EBPF_DATA_BUFFER_SIZE);
-    memset(scratch_buf, 0, EBPF_SCRATCH_BUFFER_SIZE);
+    if (posix_memalign((void **)&scratch_buf, EBPF_SCRATCH_BUFFER_SIZE,
+                       EBPF_DATA_BUFFER_SIZE) != 0)
+        die("scratch buf posix_memalign() failed");
+    sleep(2);
 
-    memset(&ctx, 0, sizeof(ctx));
-    ctx.footer_len = st.st_size - offset;
-    printf("Footer len: %lu\n", ctx.footer_len);
-    ctx.stage = kFooterStage;
-    strncpy((char *)&ctx.key, key, strlen(key) + 1);
-    memcpy(scratch_buf, &ctx, sizeof(ctx));
+    for (int i = 0; i < 100; i++) {
+        memset(data_buf, 0, EBPF_DATA_BUFFER_SIZE);
+        memset(scratch_buf, 0, EBPF_SCRATCH_BUFFER_SIZE);
 
-    long ret = syscall(SYS_READ_XRP, sst_fd, data_buf, 4096, offset, bpf_fd, scratch_buf);
+        memset(&ctx, 0, sizeof(ctx));
+        ctx.footer_len = st.st_size - offset;
+        printf("Footer len: %lu\n", ctx.footer_len);
+        ctx.stage = kFooterStage;
+        strncpy((char *)&ctx.key, key, strlen(key) + 1);
+        memcpy(scratch_buf, &ctx, sizeof(ctx));
 
-    printf("Return: %ld\n", ret);
-    printf("%s\n", strerror(errno));
+        bpf_fd = -1234;
+        long ret = syscall(SYS_READ_XRP, sst_fd, data_buf, 4096, offset, bpf_fd,
+                           scratch_buf);
 
-    if (ret < 0)
-        die("read_xrp() failed");
+        printf("Return: %ld\n", ret);
+        printf("%s\n", strerror(errno));
 
-    if ((out_fd = open("outfile", O_RDWR | O_CREAT | O_TRUNC, 0666)) == -1)
-        die("open() failed");
+        if (ret < 0) die("read_xrp() failed");
 
-    if (write(out_fd, data_buf, EBPF_DATA_BUFFER_SIZE) == -1)
-        die("write() failed");
+        if ((out_fd = open("outfile", O_RDWR | O_CREAT | O_TRUNC, 0666)) == -1)
+            die("open() failed");
 
-    ctx = *(struct rocksdb_ebpf_context *)scratch_buf;
+        if (write(out_fd, data_buf, EBPF_DATA_BUFFER_SIZE) == -1)
+            die("write() failed");
 
-    if (ctx.found == 1)
-        printf("Value found: %s\n", ctx.data_context.value);
-    else
-        printf("Value not found\n");
+        ctx = *(struct rocksdb_ebpf_context *)scratch_buf;
 
-    if (munmap(data_buf, EBPF_DATA_BUFFER_SIZE) != 0)
-        fprintf(stderr, "failed to munmap %p length %lu\n", data_buf, EBPF_DATA_BUFFER_SIZE);
+        if (ctx.found == 1)
+            printf("Value found: %s\n", ctx.data_context.value);
+        else
+            printf("Value not found\n");
+    }
+
+    // if (munmap(data_buf, EBPF_DATA_BUFFER_SIZE) != 0)
+    //     fprintf(stderr, "failed to munmap %p length %lu\n", data_buf,
+    //     EBPF_DATA_BUFFER_SIZE);
 
     free(scratch_buf);
 
     close(out_fd);
-    close(sst_fd);
+    // close(sst_fd);
     close(bpf_fd);
 }
